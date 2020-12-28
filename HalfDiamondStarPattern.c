6c1b02988705c43e3cc81a30b808c53cd9629acf
Mridul Tailor <71081929+MridulTailor@users.noreply.github.com>
2020-12-28 09:51:28 +0530

    Add files via upload
    Half diamond star pattern.

---
+++ HalfDiamondStarPattern.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main()
+{
+	int num_rows, row, column;
+    
+    scanf("%d", &num_rows);
+    printf("\n");
+    for(row=1; row<=num_rows-1; row++)
+    {
+    	for(column=1; column<=row; column++)
+    	{
+    		printf("*");
+		}
+		printf("\n");
+	}
+	for(row=num_rows; row>=1; row--)
+    {
+    	for(column=1; column<=row; column++)
+    	{
+    		printf("*");
+		}
+		printf("\n");
+	}
+}
+
